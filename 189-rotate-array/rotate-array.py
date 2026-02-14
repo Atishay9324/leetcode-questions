b716bed96f9e273e134940ea59d13849623d52bd
Atishay Kumar Pandey <szodin01@gmail.com>
2026-02-15 02:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 26.5 MB (62.89%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,7 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range (1,k+1):
-            nums.insert(0,nums[len(nums)-1])
-            nums.pop()
-        
+        n = len(nums)
+        k = k % n
+        temp = nums[n-k:]
+        for i in range (n-k-1, -1, -1):
+            nums[i+k]=nums[i]
+        nums[:k] = temp
